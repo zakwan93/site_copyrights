e6b0ad1c6ef79f8eeb63458f123ae6c2b1e1c4b7
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-08-03 12:11:46 -0400

    Site Copyrights gem initial commit

---
+++ lib/site_copyrights.rb
@@ -0,0 +1,7 @@
+require "site_copyrights/version"
+require "site_copyrights/renderer"
+
+module SiteCopyrights
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/site_copyrights/version.rb
@@ -0,0 +1,3 @@
+module SiteCopyrights
+  VERSION = "0.1.0"
+end
